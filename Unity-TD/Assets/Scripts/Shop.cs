6d52897d064be02628b7d6cbeb3b36678ea23b22
realTobby(kordesii) <tkattanek00@gmail.com>
2022-09-23 13:57:33 +0200

    Video9
    https://youtu.be/IMhRDTf9pBk
    Add new Tower, more range less firerate, change shop to purchase new tower

--- Unity-TD/Assets/Scripts/Shop.cs
+++ Unity-TD/Assets/Scripts/Shop.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public GameObject PREFAB_Tower0;
-
+    public GameObject PREFAB_Tower1;
 
     private void Awake()
     {
@@ -16,6 +16,7 @@ public class Shop : MonoBehaviour
     private void LoadPrefabTowers()
     {
         PREFAB_Tower0 = Resources.Load<GameObject>("Towers/Tower0");
+        PREFAB_Tower1 = Resources.Load<GameObject>("Towers/Tower1");
     }
 
     public void PurchaseTower0()
@@ -23,8 +24,13 @@ public class Shop : MonoBehaviour
         BuildManager.Instance.SelectedShopItem = PREFAB_Tower0;
     }
 
-    
+    public void PurchaseTower1()
+    {
+        BuildManager.Instance.SelectedShopItem = PREFAB_Tower1;
+    }
 
-    
+
+
+
 
 }
